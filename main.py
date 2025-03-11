0a9a7db377a0ab6142ba6f283344058c6b04dd5f
misterabs26 <mister.abs26@gmail.com>
2025-03-11 20:08:33 +0800

    Add a row below columns.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
+row1 = st.empty()
 
 with col1:
     st.image("images/photo.jpg")
@@ -12,3 +13,8 @@ with col2:
     An aspiring python developer, a data analyst, and a web developer.
     """
     st.info(content)
+
+with row1:
+    content = """Below, you can find some of the apps I have built in Python.
+    Feel free to contact me!"""
+    st.write(content)
