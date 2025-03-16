a8134be456c00090dad5f9d16e0c4b949daf8e26
misterabs26 <mister.abs26@gmail.com>
2025-03-16 12:28:09 +0800

    Fix project section

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
 import streamlit as st
+import pandas as pd
+import json
+
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
-row1 = st.empty()
+profile_section = st.container()
 
-with col1:
-    st.image("images/photo.jpg")
-with col2:
-    st.title("Gerald Ayeras")
-    content ="""
-    Hi, I am **Gerald Ayeras** from the Philippines.
-    An aspiring python developer, a data analyst, and a web developer.
+with profile_section:
+    about_me = """
+    Hi! I’m Gerald Ayeras from the Philippines, an aspiring Python developer with a strong passion for data analysis and web development.
+    Hi! I’m Gerald Ayeras from the Philippines, an aspiring Python developer with a strong passion for data analysis and web development.
+    I’m currently on an exciting journey of learning and building projects to sharpen my skills in Python and other technologies that bring ideas to life.
+
+    My goal is to develop efficient, scalable, and impactful solutions—whether that means automating processes with Python, 
+    analyzing data to uncover meaningful insights, or creating dynamic web applications. I enjoy problem-solving and constantly challenging myself with new concepts, as I believe continuous learning is key to growth in the tech industry.
+    
+    As I progress in my journey, I’m exploring frameworks, libraries, and tools that enhance my development skills. 
+    I’m particularly interested in data visualization, backend development, and front-end technologies that help create seamless user experiences.
     """
-    st.info(content)
+    st.header("About Me")
+    col1,col2 = st.columns([1.5,5])
+    with col1:
+        st.image("images/photo.jpg", use_container_width=True)
+    with col2:
+        st.subheader("Bio")
+        st.info(about_me)
 
-with row1:
-    content = """Below, you can find some of the apps I have built in Python.
-    Feel free to contact me!"""
+with st.container():
+    content = """ This portfolio is a reflection of my learning process, showcasing the projects I’ve worked on and the skills I’ve gained along the way. 
+    I’m always open to collaboration, feedback, and new opportunities, so feel free to reach out!"""
     st.write(content)
+
+df = pd.read_csv("data.csv", sep=";")
+
+with st.container():
+    st.header("Projects")
+    cols = st.columns(3,gap="large")
+    for i, row in df.iterrows():
+        col = cols[i % 3]
+        with col:
+            st.subheader(row["title"])
+            st.image(f"images/{row["image"]}",use_container_width=True)
+            st.write(row["description"])
+            with st.expander("Source Code"):
+                st.info(row['url'])
