ce13f85e9358ba7d537482745c2d691c4a7ddaad
misterabs26 <mister.abs26@gmail.com>
2025-03-18 06:56:05 +0800

    Added form in contact me page

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your email address: ",
+                               key="email")
+    user_message = st.text_area("Your message here:",
+                                key="message")
+    submit_btn = st.form_submit_button("Submit")
+
+    if submit_btn:
+        print("I was pressed!")
+
+    st.session_state
+
